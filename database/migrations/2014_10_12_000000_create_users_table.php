e27ae980f2a8a6e2d116ba0e60ee41f8e0d8a3b4
Raja Pasha Agastya Zukhruf Firdausi Sulaksana <raja.pasha.rp@gmail.com>
2021-12-28 22:57:00 +0700

    Finished migration user, camp, camp_benefit, and checkout. Also make model for each table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
+            $table->SoftDeletes();
         });
     }
 
